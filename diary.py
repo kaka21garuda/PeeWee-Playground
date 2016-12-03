83b474b747d5f79ee0e3c16fb00b5b4fedd662cf
Buka Cakrawala <kaka10garuda@gmail.com>
2016-12-03 14:30:33 -0800

    q to quit terminal

--- diary.py
+++ diary.py
@@ -1,4 +1,6 @@
+from collections import OrderedDict
 import datetime
+
 from peewee import *
 
 db = SqliteDatabase('diary.db')
@@ -15,8 +17,19 @@ def initiliaze():
     db.connect()
     db.create_tables([Entry], safe = True)
 
+
 def menu_loop():
     """show the menu"""
+    choice = None
+
+    while choice != 'q':
+        print("Enter 'q' to quit.")
+        for key, value in menu.items():
+            print("{}) {}".format(key, value.__doc__))
+        choice = raw_input('Action: ').lower().strip()
+
+        if choice in menu:
+            menu[choice]()
 
 def add_entry():
     """add an entry"""
@@ -26,6 +39,11 @@ def read_entry():
 
 def delete_entry(entry):
     """delete an entry"""
+
+menu = OrderedDict([
+    ('a', add_entry),
+    ('r', read_entry)
+])
 
 if __name__ == '__main__':
     initiliaze()
